77e2fc155096a2ab465666e65e4757cf240db468
Habib <habiburahmanpayenda@gmail.com>
2023-04-28 11:35:47 +0430

    Adds homeTable

--- src/pages/Homes/index.jsx
+++ src/pages/Homes/index.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "./homes.module.css";
 import SectionHeader from "../../components/SectionHeader";
 import HomeCreate from "../../Forms/HomeCreate";
+import HomesTable from "../../tables/HomeTable";
 
 function Homes() {
   return (
     <div className={styles.container}>
       <div className={styles.listContainer}>
         <SectionHeader title="Homes" />
-        <div className={styles.contentContainer}></div>
+        <div className={styles.contentContainer}>
+          <div style={{ flex: 9, width: "100%" }}>
+            <HomesTable />
+          </div>
+        </div>
       </div>
       <div className={styles.addNewContainer}>
         <HomeCreate />
