73214c0f0522f7c55f8d971fdd6b23bed4a9e8a9
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 11:13:21 +0430

    Adds usersSearchTable and displays it

--- src/pages/UserSearches/index.jsx
+++ src/pages/UserSearches/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import UserSearchesTable from "../../tables/UserSearchesTable";
+import styles from "./userSearches.module.css";
+import { useSelector } from "react-redux";
+import SectionHeader from "../../components/SectionHeader";
+import UserCreate from "../../Forms/UserCreate";
 
 function UserSearches() {
-  return <div>UserSearches</div>;
+  const user = useSelector((state) => state.users.showUser);
+  return (
+    <div className={styles.container}>
+      <div className={styles.list}>
+        <SectionHeader title="User Searches" />
+        <div style={{ flex: 9, width: "100%" }}>
+          {user?.user_searches?.length < 1 && (
+            <p>This user has not searched anything yet</p>
+          )}
+          <UserSearchesTable />
+        </div>
+      </div>
+      <div className={styles.addNew}>Graph</div>
+    </div>
+  );
 }
 
 export default UserSearches;
---
+++ src/tables/UserSearchesTable/columns.js
@@ -0,0 +1,14 @@
+export const UserSearchesTableComlumns = [
+  {
+    Header: "Id",
+    accessor: "id"
+  },
+  {
+    Header: "Text",
+    accessor: "term"
+  },
+  {
+    Header: "Date",
+    accessor: "created_at"
+  },
+]
