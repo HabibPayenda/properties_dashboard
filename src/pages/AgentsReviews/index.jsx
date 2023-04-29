f6a6f830e41240462ec2a9452d073eeaa11d12cf
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 21:26:10 +0430

    Adds agents reviews basic layout

--- src/pages/AgentsReviews/index.jsx
+++ src/pages/AgentsReviews/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SectionHeader from "../../components/SectionHeader";
+import UsersReviewsTable from "../../tables/UsersReviewsTable";
+import styles from "./agentsReviews.module.css";
+import { useSelector } from "react-redux";
 
 function AgentsReviews() {
-  return <div>AgentsReviews</div>;
+  const agentReviews = useSelector((state) => state.agentReviews.agentReviews);
+  return (
+    <div className={styles.container}>
+      <div className={styles.list}>
+        <SectionHeader title="Agents Reviews" />
+        <div style={{ flex: 9, width: "100%" }}>
+          {agentReviews?.length < 1 && <p>There is no agents reviews yet</p>}
+          <UsersReviewsTable />
+        </div>
+      </div>
+      <div className={styles.addNew}>Graph</div>
+    </div>
+  );
 }
 
 export default AgentsReviews;
