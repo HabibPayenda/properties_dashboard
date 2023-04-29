334ea4a8e6955345c41cbd0ce6080097ab2dbffa
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 19:01:18 +0430

    Adds basic layout to users reviews page

--- src/pages/UsersReviews/index.jsx
+++ src/pages/UsersReviews/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import UserReviewsTable from "../../tables/UsersReviewsTable";
+import SectionHeader from "../../components/SectionHeader";
+import { useSelector } from "react-redux";
+import styles from "./usersReviews.module.css";
 
 function UsersReviews() {
-  return <div>UsersReviews</div>;
+  const userReviews = useSelector((state) => state.userReviews.userReviews);
+  return (
+    <div className={styles.container}>
+      <div className={styles.list}>
+        <SectionHeader title="Users Reviews" />
+        <div style={{ flex: 9, width: "100%" }}>
+          {userReviews?.length < 1 && <p>There is no users reviews yet</p>}
+          <UserReviewsTable />
+        </div>
+      </div>
+      <div className={styles.addNew}>Graph</div>
+    </div>
+  );
 }
 
 export default UsersReviews;
