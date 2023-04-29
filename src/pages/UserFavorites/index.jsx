35ab2d341daff8b6c0636b4183eedd5fda810e2f
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 12:07:27 +0430

    Adds basic layout to userFavorites page

--- src/pages/UserFavorites/index.jsx
+++ src/pages/UserFavorites/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./userFavorites.module.css";
+import UserViewsTable from "../../tables/UserViewsTable";
+import SectionHeader from "../../components/SectionHeader";
+import { useSelector } from "react-redux";
 
 function UserFavorites() {
-  return <div>UserFavorites</div>;
+  const user = useSelector((state) => state.users.showUser);
+  return (
+    <div className={styles.container}>
+      <div className={styles.list}>
+        <SectionHeader title="User Favorites" />
+        <div style={{ flex: 9, width: "100%" }}>
+          {user?.user_favorites?.length < 1 && (
+            <p>This user has not added anything to favorites yet</p>
+          )}
+          <UserViewsTable />
+        </div>
+      </div>
+      <div className={styles.addNew}>Graph</div>
+    </div>
+  );
 }
 
 export default UserFavorites;
