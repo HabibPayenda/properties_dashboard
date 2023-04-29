acc1d9d9d54027967d51f4f02823a9f1ebca8882
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 17:18:28 +0430

    Adds userReviews table

---
+++ src/tables/UsersReviewsTable/columns.js
@@ -0,0 +1,22 @@
+export const UserReviewsTableComlumns = [
+  {
+    Header: "Id",
+    accessor: "id"
+  },
+  {
+    Header: "Agent ID",
+    accessor: "agent_id"
+  },
+  {
+    Header: "Apointment ID",
+    accessor: "appointment_id"
+  },
+  {
+    Header: "Review ID",
+    accessor: "review_id"
+  },
+  {
+    Header: "Date",
+    accessor: "created_at"
+  },
+]
