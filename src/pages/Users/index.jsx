874c8f38811a913beac687889351794441aae6db
Habib <habiburahmanpayenda@gmail.com>
2023-04-29 08:19:05 +0430

    Adds basic layout to users page

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import SectionHeader from "../../components/SectionHeader";
+import styles from "./users.module.css";
 
 function Users() {
   return (
-    <div>Users</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.list}>
+        <SectionHeader title="Users" />
+        <div style={{ flex: 9, width: "100%" }}></div>
+      </div>
+      <div className={styles.addNew}></div>
+    </div>
+  );
 }
 
-export default Users
+export default Users;
