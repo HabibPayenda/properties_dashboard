d69e4c40edf76bfffe0026c6ac2aff1a24ba4328
Habib <habiburahmanpayenda@gmail.com>
2023-05-02 12:41:20 +0430

    Adds toast messages in cars slice

--- src/data/CarsSlice/index.js
+++ src/data/CarsSlice/index.js
@@ -1,59 +1,54 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import PropertiesApi from '../../utils/api/propertiesApi';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import PropertiesApi from "../../utils/api/propertiesApi";
 
-
-
-export const getAllCars = createAsyncThunk(
-  'cars/getAllCars',
-  async () => {
-    // Code 
-    try {
-      const result = await PropertiesApi.get('/cars', {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+export const getAllCars = createAsyncThunk("cars/getAllCars", async () => {
+  // Code
+  try {
+    const result = await PropertiesApi.get("/cars", {
+      onUploadProgress: (progress) => {
+        if (progress.loaded / progress.total === 1) {
+        }
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-);
-export const getCar = createAsyncThunk(
-  'cars/getCar',
-  async (id) => {
-    // Code 
-    console.log(id)
-    try {
-      const result = await PropertiesApi.get(`cars/${id}`, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+});
+export const getCar = createAsyncThunk("cars/getCar", async (id) => {
+  // Code
+  console.log(id);
+  try {
+    const result = await PropertiesApi.get(`cars/${id}`, {
+      onUploadProgress: (progress) => {
+        if (progress.loaded / progress.total === 1) {
+        }
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-);
+});
 export const addCar = createAsyncThunk(
-  'cars/addCar',
-  async ({owner_name, property_id}) => {
-
+  "cars/addCar",
+  async ({ owner_name, property_id }) => {
     try {
-      const result = await PropertiesApi.post('/cars', {owner_name: owner_name, property_id: property_id * 1}, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
+      const result = await PropertiesApi.post(
+        "/cars",
+        { owner_name: owner_name, property_id: property_id * 1 },
+        {
+          onUploadProgress: (progress) => {
+            if (progress.loaded / progress.total === 1) {
+            }
+          },
+        }
+      );
       return result.data;
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return error;
     }
   }
@@ -64,15 +59,14 @@ const initialState = {
   showCar: {},
   token: null,
   noToken: null,
-  loading: 'idle',
+  loading: "idle",
   role: null,
 };
 
 export const CarsSlice = createSlice({
-  name: 'cars',
+  name: "cars",
   initialState,
   extraReducers: (builder) => {
-
     builder.addCase(getAllCars.fulfilled, (state, action) => {
       // Code
       state.cars = action.payload.cars;
@@ -84,7 +78,10 @@ export const CarsSlice = createSlice({
 
     builder.addCase(addCar.fulfilled, (state, action) => {
       // Code
-      state.cars = [...state.cars, action.payload.car]
+      state.cars = [...state.cars, action.payload.car];
+      toast.success("Car added successfully.", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     });
   },
 });
