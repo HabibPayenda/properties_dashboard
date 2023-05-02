3f7a4259b9e4227eef73d7b2e249d90e9f9dff13
Habib <habiburahmanpayenda@gmail.com>
2023-05-02 12:40:41 +0430

    Adds toast messages in agents slice

--- src/data/agentsSlice/index.js
+++ src/data/agentsSlice/index.js
@@ -1,49 +1,49 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import PropertiesApi from '../../utils/api/propertiesApi';
-import months from '../../utils/months';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import PropertiesApi from "../../utils/api/propertiesApi";
+import months from "../../utils/months";
 
-export const signIn = createAsyncThunk(
-  'agent/signIn',
-  async (data) => {
-   // Code 
-   try {
-    const result = await PropertiesApi.post('/admins/login', {name: data.name, password: data.password}, {
-      onUploadProgress: (progress) => {
-        if (progress.loaded / progress.total === 1) {
-         
-        }
-      },
-    });
-    localStorage.setItem('admin', JSON.stringify(result.data.admin))
-    localStorage.setItem('token', JSON.stringify(result.data.token))
-    return result.data
+export const signIn = createAsyncThunk("agent/signIn", async (data) => {
+  // Code
+  try {
+    const result = await PropertiesApi.post(
+      "/admins/login",
+      { name: data.name, password: data.password },
+      {
+        onUploadProgress: (progress) => {
+          if (progress.loaded / progress.total === 1) {
+          }
+        },
+      }
+    );
+    localStorage.setItem("admin", JSON.stringify(result.data.admin));
+    localStorage.setItem("token", JSON.stringify(result.data.token));
+    return result.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return error;
   }
-  },
-);
-
-export const localSignIn = createAsyncThunk('user/localSignIn', async () => {
- // Code 
- const token = localStorage.getItem('token')
- const admin = localStorage.getItem('admin')
- return {token, admin};
 });
 
-export const signOut = createAsyncThunk('user/signOut', async () => {
- // Code 
- localStorage.removeItem('admin')
- localStorage.removeItem('token')
- return null;
+export const localSignIn = createAsyncThunk("user/localSignIn", async () => {
+  // Code
+  const token = localStorage.getItem("token");
+  const admin = localStorage.getItem("admin");
+  return { token, admin };
+});
+
+export const signOut = createAsyncThunk("user/signOut", async () => {
+  // Code
+  localStorage.removeItem("admin");
+  localStorage.removeItem("token");
+  return null;
 });
 
 export const getAllAgents = createAsyncThunk(
-  'agents/getAllAgents',
+  "agents/getAllAgents",
   async () => {
-    // Code 
+    // Code
     try {
-      const result = await PropertiesApi.get('/agents', {
+      const result = await PropertiesApi.get("/agents", {
         onUploadProgress: (progress) => {
           if (progress.loaded / progress.total === 1) {
           }
@@ -51,71 +51,75 @@ export const getAllAgents = createAsyncThunk(
       });
       return result.data;
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return error;
     }
   }
 );
-export const getAgent = createAsyncThunk(
-  'agents/getAgent',
-  async (id) => {
-    // Code 
-    console.log(id)
-    try {
-      const result = await PropertiesApi.get(`agents/${id}`, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+export const getAgent = createAsyncThunk("agents/getAgent", async (id) => {
+  // Code
+  console.log(id);
+  try {
+    const result = await PropertiesApi.get(`agents/${id}`, {
+      onUploadProgress: (progress) => {
+        if (progress.loaded / progress.total === 1) {
+        }
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-);
+});
 export const addAgent = createAsyncThunk(
-  'agents/addAgent',
-  async ({name, hire_date, status, admin_id}) => {
-    // Code 
+  "agents/addAgent",
+  async ({ name, hire_date, status, admin_id }) => {
+    // Code
     // console.log("data is ", data)
     try {
-      const result = await PropertiesApi.post('/agents', {name: name, hire_date: hire_date, status: status, admin_id: admin_id * 1}, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
+      const result = await PropertiesApi.post(
+        "/agents",
+        {
+          name: name,
+          hire_date: hire_date,
+          status: status,
+          admin_id: admin_id * 1,
         },
-      });
+        {
+          onUploadProgress: (progress) => {
+            if (progress.loaded / progress.total === 1) {
+            }
+          },
+        }
+      );
       return result.data;
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return error;
     }
   }
 );
-
-
 
 const initialState = {
   agents: [],
   showAgent: {},
   token: null,
   noToken: null,
-  loading: 'idle',
+  loading: "idle",
   role: null,
   chartMonths: [],
-  chartData: []
+  chartData: [],
 };
 
 export const agentsSlice = createSlice({
-  name: 'agents',
+  name: "agents",
   initialState,
   extraReducers: (builder) => {
     builder.addCase(signIn.fulfilled, (state, action) => {
-     // Code
-     state.token = action.payload.token;
-     state.admin = action.payload.admin;
+      // Code
+      state.token = action.payload.token;
+      state.admin = action.payload.admin;
     });
 
     builder.addCase(localSignIn.fulfilled, (state, action) => {
@@ -135,25 +139,24 @@ export const agentsSlice = createSlice({
       state.agents = action.payload.agents;
       let count = 0;
       let index;
-      action.payload.agents.forEach(agent => {
-        const month = new Date(agent.created_at.slice(0, -1)).getMonth()
-        const monthText = months[month]
-        
+      action.payload.agents.forEach((agent) => {
+        const month = new Date(agent.created_at.slice(0, -1)).getMonth();
+        const monthText = months[month];
 
-        if(state.chartMonths.indexOf(monthText) == -1) {
+        if (state.chartMonths.indexOf(monthText) == -1) {
           count = 0;
           state.chartMonths.push(monthText);
-          console.log('index to be : ', state.chartMonths.indexOf(monthText))
+          console.log("index to be : ", state.chartMonths.indexOf(monthText));
           count += 1;
           index = state.chartMonths.indexOf(monthText);
-          
+
           state.chartData[index] = 1;
         } else {
           count += 1;
-          console.log("index is : ", index)
-            state.chartData[index] = count ;
+          console.log("index is : ", index);
+          state.chartData[index] = count;
         }
-      }); 
+      });
     });
     builder.addCase(getAgent.fulfilled, (state, action) => {
       // Code
@@ -162,7 +165,10 @@ export const agentsSlice = createSlice({
 
     builder.addCase(addAgent.fulfilled, (state, action) => {
       // Code
-      state.agents = [...state.agents, action.payload.agent]
+      state.agents = [...state.agents, action.payload.agent];
+      toast.success("Agent added successfully.", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     });
   },
 });
