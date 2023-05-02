bd2eea480dd1d9ac038f882f59f6eb5579eca801
Habib <habiburahmanpayenda@gmail.com>
2023-05-02 12:43:27 +0430

    Adds toast messages in land slice

--- src/data/LandsSlice/index.js
+++ src/data/LandsSlice/index.js
@@ -1,59 +1,54 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import PropertiesApi from '../../utils/api/propertiesApi';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import PropertiesApi from "../../utils/api/propertiesApi";
 
-
-
-export const getAllLands = createAsyncThunk(
-  'lands/getAllLands',
-  async () => {
-    // Code 
-    try {
-      const result = await PropertiesApi.get('/lands', {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+export const getAllLands = createAsyncThunk("lands/getAllLands", async () => {
+  // Code
+  try {
+    const result = await PropertiesApi.get("/lands", {
+      onUploadProgress: (progress) => {
+        if (progress.loaded / progress.total === 1) {
+        }
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-);
-export const getLand = createAsyncThunk(
-  'lands/getLand',
-  async (id) => {
-    // Code 
-    console.log(id)
-    try {
-      const result = await PropertiesApi.get(`lands/${id}`, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+});
+export const getLand = createAsyncThunk("lands/getLand", async (id) => {
+  // Code
+  console.log(id);
+  try {
+    const result = await PropertiesApi.get(`lands/${id}`, {
+      onUploadProgress: (progress) => {
+        if (progress.loaded / progress.total === 1) {
+        }
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
-);
+});
 export const addLand = createAsyncThunk(
-  'lands/addLand',
-  async ({owner_name, property_id}) => {
-
+  "lands/addLand",
+  async ({ owner_name, property_id }) => {
     try {
-      const result = await PropertiesApi.post('/lands', {owner_name: owner_name, property_id: property_id * 1}, {
-        onUploadProgress: (progress) => {
-          if (progress.loaded / progress.total === 1) {
-          }
-        },
-      });
+      const result = await PropertiesApi.post(
+        "/lands",
+        { owner_name: owner_name, property_id: property_id * 1 },
+        {
+          onUploadProgress: (progress) => {
+            if (progress.loaded / progress.total === 1) {
+            }
+          },
+        }
+      );
       return result.data;
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return error;
     }
   }
@@ -64,15 +59,14 @@ const initialState = {
   showLand: {},
   token: null,
   noToken: null,
-  loading: 'idle',
+  loading: "idle",
   role: null,
 };
 
 export const LandsSlice = createSlice({
-  name: 'lands',
+  name: "lands",
   initialState,
   extraReducers: (builder) => {
-
     builder.addCase(getAllLands.fulfilled, (state, action) => {
       // Code
       state.lands = action.payload.lands;
@@ -84,7 +78,10 @@ export const LandsSlice = createSlice({
 
     builder.addCase(addLand.fulfilled, (state, action) => {
       // Code
-      state.lands = [...state.lands, action.payload.land]
+      state.lands = [...state.lands, action.payload.land];
+      toast.success("Land added successfully.", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     });
   },
 });
