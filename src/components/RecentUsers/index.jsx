2dc1e04cb04902467a3507ac14312dcfab278fc7
Habib <habiburahmanpayenda@gmail.com>
2023-05-04 10:01:23 +0430

    Adds recent users component

---
+++ src/components/RecentUsers/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./recentUsers.module.css";
+
+function RecentUsers() {
+  return (
+    <div className={styles.container}>
+      <p>Recent Users</p>
+    </div>
+  );
+}
+
+export default RecentUsers;
