5bece53f0d1ee0015c9064e49214d67352967f1a
Habib <habiburahmanpayenda@gmail.com>
2023-05-05 13:53:25 +0430

    Adds styles to users page

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -8,20 +8,23 @@ import { getAllUsers } from "../../data/usersSlice";
 
 function Users() {
   const users = useSelector((state) => state.users.users);
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllUsers());
   }, []);
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.list}>
-        <SectionHeader title="Users" />
+        <SectionHeader
+          title="Users"
+          text="App Users Unite: How Mobile Technology is Revolutionizing the Way We Live!"
+        />
         <div style={{ flex: 9, width: "100%" }}>
           {users.length < 1 ? <p>No users yet</p> : <UsersTable />}
         </div>
-      </div>
-      <div className={styles.addNew}>
-        <UserCreate />
       </div>
     </div>
   );
