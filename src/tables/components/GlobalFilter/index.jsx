1c721c4670414accf64680860facd0d64f999241
Habib <habiburahmanpayenda@gmail.com>
2023-05-05 08:40:12 +0430

    Changes styles for global filter component

--- src/tables/components/GlobalFilter/index.jsx
+++ src/tables/components/GlobalFilter/index.jsx
@@ -4,11 +4,14 @@ import styles from "./globalFilter.module.css";
 function GlobalFilter({ filter, setFilter }) {
   return (
     <span className={styles.container}>
-      <p className={styles.label}>Search:</p>
+      <p className={styles.label}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </p>
       <input
         className={styles.input}
         value={filter || ""}
         onChange={(e) => setFilter(e.target.value)}
+        placeholder="Search here..."
       />
     </span>
   );
