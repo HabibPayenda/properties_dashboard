54b61411ba444708a46b36a1ea03d9db67ae756e
Habib <habiburahmanpayenda@gmail.com>
2023-05-05 12:54:28 +0430

    Changes layout styles in home page

--- src/pages/Homes/index.jsx
+++ src/pages/Homes/index.jsx
@@ -3,12 +3,19 @@ import styles from "./homes.module.css";
 import SectionHeader from "../../components/SectionHeader";
 import HomeCreate from "../../Forms/HomeCreate";
 import HomesTable from "../../tables/HomeTable";
+import { useSelector } from "react-redux";
 
 function Homes() {
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.listContainer}>
-        <SectionHeader title="Homes" />
+        <SectionHeader
+          title="Homes"
+          text="Embark on a Journey of Discovery: Exploring Homes That Capture the Heart and Mind"
+        />
         <div className={styles.contentContainer}>
           <div style={{ flex: 9, width: "100%" }}>
             <HomesTable />
