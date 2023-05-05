19c3471ff3516e0d443e878d3244aeed367671d5
Habib <habiburahmanpayenda@gmail.com>
2023-05-05 14:26:06 +0430

    Fixes styles in appointments page

--- src/pages/Appointments/index.jsx
+++ src/pages/Appointments/index.jsx
@@ -3,17 +3,21 @@ import styles from "./appointments.module.css";
 import SectionHeader from "../../components/SectionHeader";
 import AppointmentsTable from "../../tables/Appointments";
 import AppointmentCreate from "../../Forms/AppointmentCreate";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllAppointments } from "../../data/appointmentSlice";
 
 function Appointments() {
   const dispatch = useDispatch();
 
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
+
   useEffect(() => {
     dispatch(getAllAppointments());
   }, [dispatch]);
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.listContainer}>
         <SectionHeader title="Appointments" />
         <div className={styles.contentContainer}>
@@ -21,9 +25,6 @@ function Appointments() {
             <AppointmentsTable />
           </div>
         </div>
-      </div>
-      <div className={styles.addNewContainer}>
-        <AppointmentCreate />
       </div>
     </div>
   );
