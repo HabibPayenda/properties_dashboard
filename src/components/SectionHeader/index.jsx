8e6b4f65e04e72859e3251dcf10e65dc37527e90
Habib <habiburahmanpayenda@gmail.com>
2023-05-05 10:45:19 +0430

    Adds styles to section header

--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styles from "./sectionHeader.module.css";
-function SectionHeader({ title }) {
+function SectionHeader({ title, text }) {
   return (
     <div className={styles.container}>
-      <h1>{title}</h1>
+      <div className={styles.headerContainer}>
+        <div className={styles.badgeContainer}></div>
+        <div className={styles.badgeCorner}></div>
+        <div className={styles.leftBackground}></div>
+        <div className={styles.rightBackground}></div>
+        <div className={styles.rightBadge}></div>
+        <div className={styles.rightBadgeCorner}></div>
+        <div className={styles.tilesContainer}>
+          <div className={styles.tile}></div>
+          <div className={styles.tile}></div>
+          <div className={styles.tile}></div>
+          <div className={styles.tile}></div>
+        </div>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.title}>{text}</p>
+      </div>
     </div>
   );
 }
