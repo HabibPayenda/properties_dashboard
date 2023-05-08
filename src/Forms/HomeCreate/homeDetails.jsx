0753594c4985e4cf3ef3d4fde0edc436acf8550c
Habib <habiburahmanpayenda@gmail.com>
2023-05-08 11:04:36 +0430

    Adds home details form and uses useMultiStep form hook in create home

---
+++ src/Forms/HomeCreate/homeDetails.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import TextInput from "../../components/TextInput";
+import FormSelect from "../../components/FromSelect";
+
+function HomeDetailsForm({
+  formik,
+  styles,
+  agents,
+  propertyManagers,
+  title,
+  text,
+}) {
+  return (
+    <>
+      <div className={styles.formDetails}>
+        <h2 className={styles.formDetailsTitle}>{title}</h2>
+        <p className={styles.formDetailsText}>{text}</p>
+      </div>
+      <div className={styles.inputsContainer}>
+        <TextInput
+          label="Owner:"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Owner Name"
+          className={styles.input}
+          value={formik.values.owner_name}
+          id="owner_name"
+          type="text"
+          errors={formik.errors.owner_name}
+          touched={formik.touched.owner_name}
+        />
+
+        <TextInput
+          label="Name:"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Home Name"
+          className={styles.input}
+          value={formik.values.name}
+          id="name"
+          type="text"
+          errors={formik.errors.name}
+          touched={formik.touched.name}
+        />
+
+        <TextInput
+          label="Description:"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Home Description"
+          className={styles.input}
+          value={formik.values.description}
+          id="description"
+          type="text"
+          errors={formik.errors.description}
+          touched={formik.touched.description}
+        />
+
+        <FormSelect
+          id="status"
+          value={formik.values.status}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          label="Status:"
+          titles={["Available", "Not Available"]}
+          values={["available", "not_available"]}
+          errors={formik.errors.status}
+          touched={formik.touched.status}
+          setFieldValue={formik.setFieldValue}
+          setFieldTouched={formik.setFieldTouched}
+        />
+
+        <FormSelect
+          value={formik.values.agent_id}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          label="Agent:"
+          titles={agents.map((agent) => agent.name)}
+          values={agents.map((agent) => agent.id)}
+          errors={formik.errors.agent_id}
+          touched={formik.touched.agent_id}
+          id="agent_id"
+          setFieldValue={formik.setFieldValue}
+          setFieldTouched={formik.setFieldTouched}
+        />
+        <FormSelect
+          value={formik.values.property_manager_id}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          label="Manager:"
+          titles={propertyManagers.map(
+            (propertyManager) => propertyManager.name
+          )}
+          values={propertyManagers.map((propertyManager) => propertyManager.id)}
+          errors={formik.errors.property_manager_id}
+          touched={formik.touched.property_manager_id}
+          id="property_manager_id"
+          setFieldValue={formik.setFieldValue}
+          setFieldTouched={formik.setFieldTouched}
+        />
+      </div>
+    </>
+  );
+}
+
+export default HomeDetailsForm;
--- src/Forms/HomeCreate/index.jsx
+++ src/Forms/HomeCreate/index.jsx
@@ -7,6 +7,10 @@ import FormSelect from "../../components/FromSelect";
 import { useDispatch, useSelector } from "react-redux";
 import FormBtn from "../../components/FormBtn";
 import { addHome } from "../../data/homeSlice";
+import useMultistepForm from "../../hooks/useMultistepForm";
+import FormPaginationBtn from "../../components/FormPaginationBtn";
+import FormPageInfo from "../../components/FormPageInfo";
+import HomeDetailsForm from "./homeDetails";
 
 function HomeCreate() {
   const agents = useSelector((state) => state.agents.agents);
@@ -38,104 +42,50 @@ function HomeCreate() {
     onSubmit: handleFormSubmit,
   });
 
-  const showErrors = () => {
-    for (let error in formik.errors) {
-      if (formik.touched[error]) {
-        return <p>{formik.errors[error]}</p>;
-      }
-    }
-  };
-
-  console.log(formik.values);
+  const { currentPage, isLastPage, nextPage, previousPage, currentPageIndex } =
+    useMultistepForm([
+      <HomeDetailsForm
+        title="Add New Home to the System"
+        text="Effortlessly Manage Home Information: Perfecting Your Team's Efficiency!"
+        formik={formik}
+        agents={agents}
+        propertyManagers={propertyManagers}
+        styles={styles}
+      />,
+    ]);
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Create New Home</h2>
-      <form className={styles.form} onSubmit={formik.handleSubmit}>
-        <TextInput
-          label="Owner:"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="Owner Name"
-          className={styles.input}
-          value={formik.values.owner_name}
-          id="owner_name"
-          type="text"
-          errors={formik.errors.owner_name}
-          touched={formik.touched.owner_name}
-        />
-
-        <TextInput
-          label="Name:"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="Home Name"
-          className={styles.input}
-          value={formik.values.name}
-          id="name"
-          type="text"
-          errors={formik.errors.name}
-          touched={formik.touched.name}
-        />
-
-        <TextInput
-          label="Description:"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          placeholder="Home Description"
-          className={styles.input}
-          value={formik.values.description}
-          id="description"
-          type="text"
-          errors={formik.errors.description}
-          touched={formik.touched.description}
-        />
-
-        <FormSelect
-          id="status"
-          value={formik.values.status}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          label="Status:"
-          titles={["Available", "Not Available"]}
-          values={["available", "not_available"]}
-          errors={formik.errors.status}
-          touched={formik.touched.status}
-          setFieldValue={formik.setFieldValue}
-          setFieldTouched={formik.setFieldTouched}
-        />
-
-        <FormSelect
-          value={formik.values.agent_id}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          label="Agent:"
-          titles={agents.map((agent) => agent.name)}
-          values={agents.map((agent) => agent.id)}
-          errors={formik.errors.agent_id}
-          touched={formik.touched.agent_id}
-          id="agent_id"
-          setFieldValue={formik.setFieldValue}
-          setFieldTouched={formik.setFieldTouched}
-        />
-        <FormSelect
-          value={formik.values.property_manager_id}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          label="Manager:"
-          titles={propertyManagers.map(
-            (propertyManager) => propertyManager.name
-          )}
-          values={propertyManagers.map((propertyManager) => propertyManager.id)}
-          errors={formik.errors.property_manager_id}
-          touched={formik.touched.property_manager_id}
-          id="property_manager_id"
-          setFieldValue={formik.setFieldValue}
-          setFieldTouched={formik.setFieldTouched}
-        />
-        <FormBtn title="Create" onClick={() => handleFormSubmit()} />
-      </form>
-      <div>{showErrors()}</div>
+      <div className={styles.sidebar}>
+        <div className={styles.sidbarHeader}>
+          <i className={["fa-solid fa-igloo"]}> </i>
+          <h1 className={styles.logoText}>PAPI</h1>
+        </div>
+        <div className={styles.sidebarStepsContainer}>
+          <FormPageInfo
+            title="Home Details"
+            isCurrentPage={true}
+            pageNumber={1}
+          />
+          <FormPageInfo
+            title="Home Address"
+            isCurrentPage={currentPageIndex >= 1}
+            pageNumber={2}
+          />
+        </div>
+      </div>
+      <div className={styles.formContent}>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          {currentPage}
+        </form>
+        <div className={styles.btnContainer}>
+          <FormPaginationBtn title="Previous" onClick={() => previousPage()} />
+          <FormPaginationBtn
+            title={isLastPage ? "Finish" : "Next"}
+            onClick={() => nextPage(formik)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
