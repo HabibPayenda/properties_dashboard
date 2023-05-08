5112a3a4cce56827dcd6114658b30b901e4c42a1
Habib <habiburahmanpayenda@gmail.com>
2023-05-08 09:14:48 +0430

    Adds details to personDetailsHeader component

--- src/components/PersonDetailsHeader/index.jsx
+++ src/components/PersonDetailsHeader/index.jsx
@@ -9,9 +9,22 @@ function PersonDetailsHeader() {
       </div>
       <div className={styles.detailsContainer}>
         <div className={styles.hiddenCirle}></div>
-        <div className={styles.detailsContent}>Content</div>
-        <div className={styles.detailsContent}>Content</div>
-        <div className={styles.detailsContent}>Content</div>
+        <div className={styles.detailsContentContainer}>
+          <div className={styles.detailsContent}>
+            <p className={styles.detailsTitle}>Habib Payenda</p>
+            <p className={styles.detailsItem}>last_active:</p>
+          </div>
+          <div className={styles.detailsContent}>
+            <p className={styles.detailsTitle}>Contact</p>
+            <p className={styles.detailsItem}>Mobile:</p>
+            <p className={styles.detailsItem}>Email:</p>
+          </div>
+          <div className={styles.detailsContent}>
+            <p className={styles.detailsTitle}>Address</p>
+            <p className={styles.detailsItem}>City:</p>
+            <p className={styles.detailsItem}>District:</p>
+          </div>
+        </div>
       </div>
     </div>
   );
