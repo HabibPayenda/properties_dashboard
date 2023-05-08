1dd298645f721b57669894fcf6e4544bb975eaf0
Habib <habiburahmanpayenda@gmail.com>
2023-05-08 21:24:11 +0430

    Adds PropertyDetailsComponent

---
+++ src/components/PropertyDetailsCard/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./propertyDetailsCard.module.css";
+
+function PropertyDetailsCard() {
+  return <div className={styles.container}>PropertyDetailsCard</div>;
+}
+
+export default PropertyDetailsCard;
