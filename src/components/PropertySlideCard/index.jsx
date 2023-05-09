bc0bf2c00a7a369e93d81a3fcb1b7e167c276228
Habib <habiburahmanpayenda@gmail.com>
2023-05-09 14:51:54 +0430

    Adds layout styles to propertySlideCard

--- src/components/PropertySlideCard/index.jsx
+++ src/components/PropertySlideCard/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from "./propertySlideCard.module.css";
+
+import homeImage from "../../assets/home.jpg";
 function PropertySlideCard({ title }) {
-  return <div className={styles.container}>{title}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={homeImage} alt="" />
+      </div>
+      <div className={styles.detailsContainer}>
+        <div className={styles.header}>Header</div>
+        <div className={styles.content}>Content</div>
+        <div className={styles.footer}>
+          <p className={styles.editBtn}>Edit</p>
+          <p className={styles.deleteBtn}>Delete</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PropertySlideCard;
