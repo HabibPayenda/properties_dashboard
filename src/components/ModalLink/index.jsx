d92501e0b283d74bea85ea7f10ec3e40d58fc136
Habib <habiburahmanpayenda@gmail.com>
2023-05-09 04:47:07 +0430

    Adds styles to modal link component

--- src/components/ModalLink/index.jsx
+++ src/components/ModalLink/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./modalLink.module.css";
 
-function ModalLink({ title, onClick }) {
+function ModalLink({ title }) {
   return (
-    <div onClick={onClick}>
+    <div className={styles.container}>
       <i class="fa-solid fa-plus"></i> {title}
     </div>
   );
