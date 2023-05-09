13cbbf1807b68609b7d84534833c3fc8ab38e413
Habib <habiburahmanpayenda@gmail.com>
2023-05-09 11:47:44 +0430

    Adds property slide card component

---
+++ src/components/PropertySlideCard/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./propertySlideCard.module.css";
+function PropertySlideCard() {
+  return <div className={styles.container}>PropertySlideCard</div>;
+}
+
+export default PropertySlideCard;
