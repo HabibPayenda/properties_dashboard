acc173cf48c7fb476d6a9f88882975d74a132cf3
Habib <habiburahmanpayenda@gmail.com>
2023-05-09 15:38:09 +0430

    Adds more layout styles to propertySlideCard content

--- src/components/PropertySlideCard/index.jsx
+++ src/components/PropertySlideCard/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./propertySlideCard.module.css";
 
 import homeImage from "../../assets/home.jpg";
-function PropertySlideCard({ title }) {
+function PropertySlideCard({ length, width, to_sun, color }) {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
@@ -10,7 +10,44 @@ function PropertySlideCard({ title }) {
       </div>
       <div className={styles.detailsContainer}>
         <div className={styles.header}>Header</div>
-        <div className={styles.content}>Content</div>
+        <div className={styles.content}>
+          <div className={styles.contentColumn}>
+            <div className={styles.contentItem}>
+              <p>Length:</p>
+              <p>{length ? `${length} m` : "6 m"}</p>
+            </div>
+            <div className={styles.contentItem}>
+              <p>Width:</p>
+              <p>{width ? `${width} m` : "4 m"}</p>
+            </div>
+          </div>
+          <div className={styles.contentColumn}>
+            <div className={styles.contentItem}>
+              <p>To sun:</p>
+              <p>{to_sun || "Yes"}</p>
+            </div>
+            <div className={styles.contentItem}>
+              <p>Windows:</p>
+              <p>1</p>
+            </div>
+          </div>
+          <div className={styles.contentColumn}>
+            <div className={styles.contentItem}>
+              Color:
+              <div
+                style={{
+                  height: "10px",
+                  width: "10px",
+                  backgroundColor: color || "black",
+                }}
+              ></div>
+            </div>
+            <div className={styles.contentItem}>
+              <p>Cupboards:</p>
+              <p>Yes</p>
+            </div>
+          </div>
+        </div>
         <div className={styles.footer}>
           <p className={styles.editBtn}>Edit</p>
           <p className={styles.deleteBtn}>Delete</p>
