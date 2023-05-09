0bed148f36bf095707caf312b5a08bcc14ff7fa2
Habib <habiburahmanpayenda@gmail.com>
2023-05-09 12:07:12 +0430

    Adds icons to main slider component

--- src/Sliders/index.jsx
+++ src/Sliders/index.jsx
@@ -10,9 +10,20 @@ function MainSlider({
   const generateFooterContent = () => {
     let content = slides.map((slide, index) => {
       if (index === currentSlideIndex) {
-        return <p>a</p>;
+        return (
+          <div className={styles.footerCircleContainer}>
+            <i
+              style={{ color: "blueviolet" }}
+              className="fa-solid fa-circle"
+            ></i>
+          </div>
+        );
       } else {
-        return <p>n</p>;
+        return (
+          <div className={styles.footerCircleContainer}>
+            <i className="fa-solid fa-circle"></i>
+          </div>
+        );
       }
     });
     return content;
@@ -26,11 +37,11 @@ function MainSlider({
     <div className={styles.container}>
       <div className={styles.contentContainer}>
         <p className={styles.btn} onClick={() => previousSlide()}>
-          prv
+          <i className="fa-solid fa-arrow-left"></i>
         </p>
         <div className={styles.content}>{currentSlide}</div>
         <p className={styles.btn} onClick={() => nextSlide()}>
-          next
+          <i className="fa-solid fa-arrow-right"></i>
         </p>
       </div>
       <div className={styles.footer}>{generateFooterContent()}</div>
