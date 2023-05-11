c2c9d73fc823e7f37dcaa7ed8380abe1b9088259
Habib <habiburahmanpayenda@gmail.com>
2023-05-11 13:19:17 +0430

    Adds basic styles for restriction list and restriction item components

--- src/components/RestrictionsList/index.jsx
+++ src/components/RestrictionsList/index.jsx
@@ -3,6 +3,9 @@ import RestrictionItem from "../RestrictionItem";
 import styles from "./restrictionsList.module.css";
 function RestrictionsList({ restrictions }) {
   const renderRestrictions = () => {
+    if (restrictions?.length < 1) {
+      return <h1>No restrictions added for this home</h1>;
+    }
     let toRender = restrictions?.map((item) => {
       return <RestrictionItem restriction={item} />;
     });
