976f225b0948c350e095174fb21f3fd49b1f6bf3
Habib <habiburahmanpayenda@gmail.com>
2023-05-11 12:59:39 +0430

    Adds styles to AmenitiesList and an AmenityItem components

--- src/components/AmenitiesList/index.jsx
+++ src/components/AmenitiesList/index.jsx
@@ -3,13 +3,16 @@ import styles from "./amenitiesList.module.css";
 import AmenityItem from "../AmenityItem";
 function AmenitiesList({ amenities }) {
   const renderAmenities = () => {
+    if (amenities?.length < 1) {
+      return <h1>No Amenities</h1>;
+    }
     let toRender = amenities.map((item) => {
       return <AmenityItem amenity={item} />;
     });
 
     return toRender;
   };
-  return <div style={styles.container}>{renderAmenities()}</div>;
+  return <div className={styles.container}>{renderAmenities()}</div>;
 }
 
 export default AmenitiesList;
--- src/components/AmenityItem/index.jsx
+++ src/components/AmenityItem/index.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import styles from "./amenityItem.module.css";
 function AmenityItem({ amenity }) {
+  console.log("amenity", amenity);
   return (
     <div className={styles.container}>
       <div className={styles.nameContainer}>
         <p className={styles.name}>{amenity?.name}</p>
       </div>
       <div className={styles.descriptionContainer}>
-        <p className={styles.description}>{amenity?.description}</p>
-      </div>
-      <div className={styles.details}>
-        <p className={styles.fee}>{amenity?.fee}</p>
-        <p className={styles.feeDuration}>{amenity?.fee_duration}</p>
+        <p aria-multiline={true} className={styles.description}>
+          {amenity?.description}
+        </p>
+        <div className={styles.details}>
+          <p className={styles.detailsItem}>
+            <i className="fa-solid fa-coins"></i>
+            {amenity?.fee}
+          </p>
+          <p className={styles.detailsItem}>
+            <i className="fa-solid fa-clock"></i>
+            {amenity?.fee_duration}
+          </p>
+        </div>
       </div>
     </div>
   );
