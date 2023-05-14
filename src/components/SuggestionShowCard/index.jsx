a241f673a40ac1df9ad89dff0f44995c22a7df42
GhotaiPayenda <ghotaipayenda43@gmail.com>
2023-05-14 11:16:44 +0430

    Adds SuggestionShowCard

---
+++ src/components/SuggestionShowCard/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './suggestionShowCard.module.css'
+
+function SuggestionShowCard() {
+  return (
+    <div className={styles.container}>SuggestionShowCard</div>
+  )
+}
+
+export default SuggestionShowCard
