7044bc3af446c834be324cc4933ec71408e559ef
GhotaiPayenda <ghotaipayenda43@gmail.com>
2023-05-15 08:58:02 +0430

    Shows data in suggestionCard component

--- src/components/SuggestionShowCard/index.jsx
+++ src/components/SuggestionShowCard/index.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import styles from "./suggestionShowCard.module.css";
 
-function SuggestionShowCard() {
+function SuggestionShowCard({ suggestion }) {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.header}>
         <div className={styles.img}></div>
         <div>
-          <h4>Susan Wong</h4>
+          <h4>{suggestion?.user?.name || "Susan Wong"}</h4>
           <span>Developer</span>
         </div>
-
-        <p>
-          <i className="fa-solid fa-star"></i>4.4 / 5
-        </p>
       </div>
-
+      <p className={styles.title}>{suggestion?.title || "Title"}</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto qui
-        reprehenderit temporibus corrupti iure ratione debitis! Consectetur
-        quaerat quo facilis odio, quos, deleniti similique quam nisi cumque
-        reprehenderit obcaecati.
+        {suggestion?.description ||
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto qui reprehenderit temporibus corrupti iure ratione debitis! Consectetur quaerat quo facilis odio, quos, deleniti similique quam nisi cumque"}
       </p>
     </div>
   );
