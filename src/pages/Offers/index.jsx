46eac879e281265f7eb73ed6c7319c6c0c79318b
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 18:24:22 +0430

    Changes layout in Offers page

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./offers.module.css";
+import SectionHeader from "../../components/SectionHeader";
 
 function Offers() {
-  return <div className={styles.container}></div>;
+  const dispatch = useDispatch();
+
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
+      <div className={styles.contentContainer}>
+        <SectionHeader title="Offers" text="All offers list" />
+        <div style={{ width: "100%" }}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Offers;
