dba44a42229bb0683a99923e4f06fad774da422e
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 17:13:07 +0430

    Adds basic layout to cars, warehouses and lands pages

--- src/pages/Cars/index.jsx
+++ src/pages/Cars/index.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./cars.module.css";
+import SectionHeader from "../../components/SectionHeader";
+import { getAllCars } from "../../data/CarsSlice";
 
 function Cars() {
-  return <div>Cars</div>;
+  const dispatch = useDispatch();
+
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
+
+  useEffect(() => {
+    dispatch(getAllCars());
+  }, []);
+
+  return (
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
+      <div className={styles.contentContainer}>
+        <SectionHeader title="Cars" text="All regestered cars" />
+        <div style={{ width: "100%" }}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Cars;
--- src/pages/Lands/index.jsx
+++ src/pages/Lands/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./lands.module.css";
+import SectionHeader from "../../components/SectionHeader";
 
 function Lands() {
-  return <div>Lands</div>;
+  const dispatch = useDispatch();
+
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
+      <div className={styles.contentContainer}>
+        <SectionHeader title="Lands" text="All regestered lands" />
+        <div style={{ width: "100%" }}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Lands;
