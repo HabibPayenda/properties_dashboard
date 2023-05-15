7fdad86556f3505278297ca4a42eaa901a054959
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 15:36:12 +0430

    Adds UserEditForm and fields

---
+++ src/Forms/UserEditForm/UserAddressEditForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import TextInput from "../../components/TextInput";
+
+function UserAddressEditForm({ formik, styles, title, text }) {
+  return (
+    <>
+      <div className={styles.formDetails}>
+        <h2 className={styles.formDetailsTitle}>{title}</h2>
+        <p className={styles.formDetailsText}>{text}</p>
+      </div>
+      <div className={styles.inputsContainer}>
+        <TextInput
+          label="Province"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="User Province"
+          className={styles.input}
+          value={formik.values.province}
+          id="province"
+          type="text"
+          errors={formik.errors.province}
+          touched={formik.touched.province}
+        />
+        <TextInput
+          label="City:"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="User City"
+          className={styles.input}
+          value={formik.values.city}
+          id="city"
+          type="text"
+          errors={formik.errors.city}
+          touched={formik.touched.city}
+        />
+        <TextInput
+          label="District:"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="User District"
+          className={styles.input}
+          value={formik.values.district}
+          id="district"
+          type="text"
+          errors={formik.errors.district}
+          touched={formik.touched.district}
+        />
+      </div>
+    </>
+  );
+}
+
+export default UserAddressEditForm;
---
+++ src/Forms/UserEditForm/index.jsx
@@ -0,0 +1,116 @@
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import styles from "userEditForm.module.css";
+import { useDispatch } from "react-redux";
+import userCreateSchema from "./userCreateSchema";
+import { addUser } from "../../data/usersSlice";
+import useMultistepForm from "../../hooks/useMultistepForm";
+import FormPageInfo from "../../components/FormPageInfo";
+import FormPaginationBtn from "../../components/FormPaginationBtn";
+import UserImageEditForm from "./UserImageEditForm";
+import UserDetailsEditForm from "./UserDetailsEditForm";
+import UserAddressEditForm from "./UserAddressEditForm";
+import UserContactEditForm from "./UserContactEditForm";
+
+function UserEditForm({ user }) {
+  const dispatch = useDispatch();
+  const [image, setImage] = useState(user?.image_url);
+
+  const handleFormSubmit = () => {
+    console.log("clicked");
+    dispatch(addUser({ ...formik.values, image: image }));
+    // formik.resetForm();
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: user?.name,
+      password: user?.password,
+      date_of_birth: user?.date_of_birth,
+      gender: user?.gender,
+      province: user?.address?.province,
+      city: user?.address?.city,
+      district: user?.address?.district,
+      phone_number_one: user?.contact?.phone_number_one,
+      email_one: user?.contact?.email_one,
+    },
+    validationSchema: userCreateSchema,
+    onSubmit: handleFormSubmit,
+  });
+
+  const { currentPage, isLastPage, nextPage, previousPage, currentPageIndex } =
+    useMultistepForm([
+      <UserImageEditForm
+        title="Edit User Image to the System"
+        text="Effortlessly Manage User Information: Perfecting Your Team's Efficiency!"
+        formik={formik}
+        styles={styles}
+        setImage={setImage}
+      />,
+      <UserDetailsEditForm
+        title="Edit User Details to the System"
+        text="Effortlessly Manage User Information: Perfecting Your Team's Efficiency!"
+        formik={formik}
+        styles={styles}
+      />,
+      <UserAddressEditForm
+        title="Edit User Address Info"
+        text="Effortlessly Manage User Information: Perfecting Your Team's Efficiency!"
+        formik={formik}
+        styles={styles}
+      />,
+      <UserContactEditForm
+        title="Edit User Contact Info"
+        text="Effortlessly Manage User Information: Perfecting Your Team's Efficiency!"
+        formik={formik}
+        styles={styles}
+      />,
+    ]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <div className={styles.sidbarHeader}>
+          <i className={["fa-solid fa-igloo"]}> </i>
+          <h1 className={styles.logoText}>PAPI</h1>
+        </div>
+        <div className={styles.sidebarStepsContainer}>
+          <FormPageInfo
+            title="Edit User Image"
+            isCurrentPage={true}
+            pageNumber={1}
+          />
+          <FormPageInfo
+            title="Edit User Details"
+            isCurrentPage={currentPageIndex >= 1}
+            pageNumber={1}
+          />
+          <FormPageInfo
+            title="Edit User Address"
+            isCurrentPage={currentPageIndex >= 2}
+            pageNumber={2}
+          />
+          <FormPageInfo
+            title="Edit User Contact"
+            isCurrentPage={currentPageIndex >= 3}
+            pageNumber={3}
+          />
+        </div>
+      </div>
+      <div className={styles.formContent}>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          {currentPage}
+        </form>
+        <div className={styles.btnContainer}>
+          <FormPaginationBtn title="Previous" onClick={() => previousPage()} />
+          <FormPaginationBtn
+            title={isLastPage ? "Finish" : "Next"}
+            onClick={() => nextPage(formik)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserEditForm;
