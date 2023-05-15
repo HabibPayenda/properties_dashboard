f3c17298dcd5b3ac8062df0e4fe36da4e6d828e6
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 18:06:54 +0430

    Adds warehouses table

--- src/pages/WareHouses/index.jsx
+++ src/pages/WareHouses/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./wareHouses.module.css";
 import SectionHeader from "../../components/SectionHeader";
+import WarehousesTable from "../../tables/WarehouseTable";
 
 function WareHouses() {
   const dispatch = useDispatch();
@@ -16,7 +17,9 @@ function WareHouses() {
     <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.contentContainer}>
         <SectionHeader title="WareHouses" text="All regestered warehouses" />
-        <div style={{ width: "100%" }}></div>
+        <div style={{ width: "100%" }}>
+          <WarehousesTable />
+        </div>
       </div>
     </div>
   );
---
+++ src/tables/WarehouseTable/columns.js
@@ -0,0 +1,38 @@
+export const WarehousesTableColumns = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Brand",
+    accessor: "brand",
+  },
+  {
+    Header: "Model",
+    accessor: "model",
+  },
+  {
+    Header: "Year",
+    accessor: "year",
+  },
+  {
+    Header: "Status",
+    accessor: "property.availability_status",
+  },
+  {
+    Header: "Mile Age",
+    accessor: "mile_age",
+  },
+  {
+    Header: "Transmission",
+    accessor: "transmission",
+  },
+  {
+    Header: "Fuel Type",
+    accessor: "fuel_type",
+  },
+  {
+    Header: "Engine Size",
+    accessor: "engine_size",
+  },
+];
