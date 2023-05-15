1b6108ed7e49c82bdf39cc98a1965fb67c268de4
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 08:25:57 +0430

    Adds pdf renderer and creates pdfs basics

--- src/pages/Trainers/index.jsx
+++ src/pages/Trainers/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { PDFViewer, BlobProvider, usePDF } from "@react-pdf/renderer";
+import React from "react";
+import PropertyManagerContractPdf from "../../Pdfs/PropertyManagerContractPdf";
+import styles from "./trainers.module.css";
 
 function Trainers() {
+  const [instance] = usePDF({ document: PropertyManagerContractPdf });
+  console.log(instance);
   return (
-    <div>Trainers</div>
-  )
+    <div className={styles.container}>
+      <PropertyManagerContractPdf />
+    </div>
+  );
 }
 
-export default Trainers
+export default Trainers;
