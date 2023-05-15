44b813a55bd7d0043fd15600b99994a27ac6ef08
Habib <habiburahmanpayenda@gmail.com>
2023-05-15 10:09:30 +0430

    Changes layout in UserViews page

--- src/pages/UserViews/index.jsx
+++ src/pages/UserViews/index.jsx
@@ -6,8 +6,11 @@ import UserViewsTable from "../../tables/UserViewsTable";
 
 function UserViews() {
   const user = useSelector((state) => state.users.showUser);
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.list}>
         <SectionHeader title="User Views" />
         <div style={{ flex: 9, width: "100%" }}>
@@ -17,7 +20,6 @@ function UserViews() {
           <UserViewsTable />
         </div>
       </div>
-      <div className={styles.addNew}>Graph</div>
     </div>
   );
 }
