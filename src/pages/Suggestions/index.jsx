0a765024b0c163deef1ff9ffbcbbaa75d2188a1a
GhotaiPayenda <ghotaipayenda43@gmail.com>
2023-05-15 08:52:53 +0430

    Adds sidebar close functionality to suggestions page

--- src/pages/Suggestions/index.jsx
+++ src/pages/Suggestions/index.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import SuggestionShowCard from "../../components/SuggestionShowCard";
 import styles from "./suggestions.module.css";
 import SectionHeader from "../../components/SectionHeader";
+import { useSelector } from "react-redux";
 
 function Suggestions() {
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
+
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <SectionHeader title="Suggestions" />
       <div className={styles.contentContainer}>
         <SuggestionShowCard />
