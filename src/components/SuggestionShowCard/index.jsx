8ba260d18fc5c62257afbbe7140fdf8bed4927d5
GhotaiPayenda <ghotaipayenda43@gmail.com>
2023-05-15 08:45:53 +0430

    Adds layout to suggestions

--- src/components/SuggestionShowCard/index.jsx
+++ src/components/SuggestionShowCard/index.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import styles from './suggestionShowCard.module.css'
+import React from "react";
+import styles from "./suggestionShowCard.module.css";
 
 function SuggestionShowCard() {
   return (
-    <div className={styles.container}>
-        <div className={styles.cardContainer}>
-            <div className={styles.header}>
-                <div className={styles.img}></div>
-                <div>
-                    <h4>Susan Wong</h4>
-                    <span>Developer</span>
-                </div>
-
-                <p>
-                    <i className="fa-solid fa-star"></i>4.4 / 5
-                </p>
-            </div>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto qui reprehenderit temporibus corrupti iure ratione debitis! Consectetur quaerat quo facilis odio, quos, deleniti similique quam nisi cumque reprehenderit obcaecati.</p>
+    <div className={styles.cardContainer}>
+      <div className={styles.header}>
+        <div className={styles.img}></div>
+        <div>
+          <h4>Susan Wong</h4>
+          <span>Developer</span>
         </div>
-       
+
+        <p>
+          <i className="fa-solid fa-star"></i>4.4 / 5
+        </p>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto qui
+        reprehenderit temporibus corrupti iure ratione debitis! Consectetur
+        quaerat quo facilis odio, quos, deleniti similique quam nisi cumque
+        reprehenderit obcaecati.
+      </p>
     </div>
-  )
+  );
 }
 
-export default SuggestionShowCard
+export default SuggestionShowCard;
--- src/pages/Suggestions/index.jsx
+++ src/pages/Suggestions/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import SuggestionShowCard from '../../components/SuggestionShowCard'
+import React from "react";
+import SuggestionShowCard from "../../components/SuggestionShowCard";
+import styles from "./suggestions.module.css";
+import SectionHeader from "../../components/SectionHeader";
 
 function Suggestions() {
   return (
-    <>
-    <div>Suggestions</div>
-    <SuggestionShowCard />
-    <SuggestionShowCard />
-    </>
-    
-  )
+    <div className={styles.container}>
+      <SectionHeader title="Suggestions" />
+      <div className={styles.contentContainer}>
+        <SuggestionShowCard />
+        <SuggestionShowCard />
+      </div>
+    </div>
+  );
 }
 
-export default Suggestions
+export default Suggestions;
