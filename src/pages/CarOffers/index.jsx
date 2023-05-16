8341fd9e3365dcfbfde04ce9aedd805c8509d2b5
Habib <habiburahmanpayenda@gmail.com>
2023-05-16 14:42:27 +0430

    Adds CarOffers page

--- src/pages/CarOffers/index.jsx
+++ src/pages/CarOffers/index.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./landOffers.module.css";
+import styles from "./carOffers.module.css";
 import SectionHeader from "../../components/SectionHeader";
 
-function LandOffers() {
+function CarOffers() {
   const dispatch = useDispatch();
 
   const isSidebarShown = useSelector(
@@ -13,7 +13,7 @@ function LandOffers() {
   return (
     <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <SectionHeader
-        title="Land offers"
+        title="Car offers"
         text="Unbeatable Property Offers: Your Guide to Finding Your Dream Home at a Great Price"
       />
       <div className={styles.contentContainer}></div>
@@ -21,4 +21,4 @@ function LandOffers() {
   );
 }
 
-export default LandOffers;
+export default CarOffers;
