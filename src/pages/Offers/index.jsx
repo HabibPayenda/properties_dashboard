b38f7bbcc200ac8872a6b08cc5b400783b58d22b
Habib <habiburahmanpayenda@gmail.com>
2023-05-16 11:51:43 +0430

    Adds ItemsCards to Offers page

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./offers.module.css";
 import SectionHeader from "../../components/SectionHeader";
+import ItemsCard from "../../components/ItemsCard";
 
 function Offers() {
   const dispatch = useDispatch();
@@ -14,12 +15,37 @@ function Offers() {
 
   return (
     <div className={isSidebarShown ? styles.container : styles.containerClose}>
+      <SectionHeader
+        title="Offers"
+        text="Unbeatable Property Offers: Your Guide to Finding Your Dream Home at a Great Price"
+      />
       <div className={styles.contentContainer}>
-        <SectionHeader
-          title="Offers"
-          text="Unbeatable Property Offers: Your Guide to Finding Your Dream Home at a Great Price"
+        <ItemsCard
+          title="Homes Offers"
+          text="Unbeatable Homes Offers"
+          icon={<i className="fa-solid fa-house"></i>}
+          to="homes"
+          total={0}
         />
-        <div style={{ width: "100%" }}></div>
+        <ItemsCard
+          title="Cars Offers"
+          text="Unbeatable Cars Offers"
+          icon={<i className="fa-solid fa-car"></i>}
+          to="/properties/homes"
+          total={0}
+        />
+        <ItemsCard
+          title="Warehouses Offers"
+          text="Unbeatable Warehouses Offers"
+          icon={<i className="fa-solid fa-building-columns"></i>}
+          to="/properties/homes"
+        />
+        <ItemsCard
+          title="Lands Offers"
+          text="Unbeatable Lands Offers"
+          icon={<i className="fa-solid fa-mountain-sun"></i>}
+          to="/properties/homes"
+        />
       </div>
     </div>
   );
