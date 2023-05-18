51d3f8f17a05133d5dce4c9aff3482203587f97d
Habib <habiburahmanpayenda@gmail.com>
2023-05-18 17:59:24 +0430

    Adds ItemCards to suggestions page

--- src/pages/Suggestions/index.jsx
+++ src/pages/Suggestions/index.jsx
@@ -3,6 +3,7 @@ import SuggestionShowCard from "../../components/SuggestionShowCard";
 import styles from "./suggestions.module.css";
 import SectionHeader from "../../components/SectionHeader";
 import { useSelector } from "react-redux";
+import ItemsCard from "../../components/ItemsCard";
 
 function Suggestions() {
   const isSidebarShown = useSelector(
@@ -13,8 +14,27 @@ function Suggestions() {
     <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <SectionHeader title="Suggestions" />
       <div className={styles.contentContainer}>
-        <SuggestionShowCard />
-        <SuggestionShowCard />
+        <ItemsCard
+          title="Users Suggestions"
+          text="These suggestions belongs to users"
+          icon={<i className="fa-solid fa-house"></i>}
+          to="users"
+          total={0}
+        />
+        <ItemsCard
+          title="Agents Suggestions"
+          text="These suggestions belongs to agents"
+          icon={<i className="fa-solid fa-house"></i>}
+          to="agents"
+          total={0}
+        />
+        <ItemsCard
+          title="Managers Suggestions"
+          text="These suggestions belongs to managers"
+          icon={<i className="fa-solid fa-house"></i>}
+          to="property_managers"
+          total={0}
+        />
       </div>
     </div>
   );
