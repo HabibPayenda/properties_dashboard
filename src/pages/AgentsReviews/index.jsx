63d841d83dbcb88163030fad16b95d9a513ddbb6
Habib <habiburahmanpayenda@gmail.com>
2023-05-18 17:52:43 +0430

    Changes layout styles for AgentsReviews page

--- src/pages/AgentsReviews/index.jsx
+++ src/pages/AgentsReviews/index.jsx
@@ -6,8 +6,11 @@ import { useSelector } from "react-redux";
 
 function AgentsReviews() {
   const agentReviews = useSelector((state) => state.agentReviews.agentReviews);
+  const isSidebarShown = useSelector(
+    (state) => state.appManagement.isSidebarShown
+  );
   return (
-    <div className={styles.container}>
+    <div className={isSidebarShown ? styles.container : styles.containerClose}>
       <div className={styles.list}>
         <SectionHeader title="Agents Reviews" />
         <div style={{ flex: 9, width: "100%" }}>
@@ -15,7 +18,6 @@ function AgentsReviews() {
           <UsersReviewsTable />
         </div>
       </div>
-      <div className={styles.addNew}>Graph</div>
     </div>
   );
 }
